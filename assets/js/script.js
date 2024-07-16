5c346a6e5f13f7272af9320ccf577fb1d775f9c9
hamid assaghli <hamid@hamids-MBP.lan>
2024-07-15 22:11:27 -0400

    update 4 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const cityFormEl = document.querySelector('#city-form');
 const cityButtonsEl = document.querySelector('#city-buttons');
 const cityInputEl = document.querySelector('#city-input');
 const cityContainerEl = document.querySelector('#city-container');
+const fiveContainer = document.querySelector('#five-days');
+const cityName = document.querySelector('#cityName');
+const weatherDate = document.querySelector('#weatherDate');
 const apiKey = 'edeb4ca36bc2cc213a04647b525557a5';
 
 let cityArray = [];
@@ -23,12 +26,14 @@ const showHistory = function () {
 };
 
 const getCityWeather = function (city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
+                    weatherDate.textContent = city;
                     displayCities(data);
                 });
             } else {
@@ -40,38 +45,62 @@ const getCityWeather = function (city) {
         });
 };
 
-const displayCities = function (cityData) {
-    if (cityData.length === 0) {
+const displayCities = function (citySt) {
+    if (citySt.length === 0) {
         cityContainerEl.textContent = 'No weather found.';
         return;
     }
-    console.log(cityData);
+    console.log(citySt);
 
 
-    const cityEl = document.createElement('div');
-    cityEl.classList = 'border border-primary p-4 text-center';
-    const now = dayjs();
-    const cityName = document.createElement('h1');
-    cityName.textContent = `${cityData.name} : ${dayjs(now).format('MM-DD-YYYY hh-mm')}`;
+
+    for (let i = 0; i < citySt.list.length; i += 8) {
+        if (i === 0) {
+            const cardC = displayForecast(citySt, 0);
+            cardC.classList = 'border-0';
+            cityContainerEl.append(cardC);
+        }
+        else {
+            let cardF = displayForecast(citySt, i);
+            fiveContainer.append(cardF);
+
+        }
+
+    }
+
+};
+
+
+const displayForecast = function (cityData, index) {
+
+    const card = document.createElement('div');
+    card.classList = 'card col-lg-2 mx-1';
+    const cardBody = document.createElement('div');
+    cardBody.classList = 'card-body';
+
+    const cityTime = document.createElement('h2');
+    cityTime, classList = 'card-title';
+    cityTime.textContent = `${dayjs(cityData.list[index].dt_txt).format('MMM-DD')}`;
 
     const weatherIcon = document.createElement('img');
-    const iconName = `${cityData.weather[0].icon}`;
+    const iconName = `${cityData.list[index].weather[0].icon}`;
     const iconUrl = `https://openweathermap.org/img/wn/${iconName}.png`
     weatherIcon.setAttribute('src', iconUrl);
 
 
     const cityTemp = document.createElement('p');
-    cityTemp.textContent = `Temp : ${cityData.main.temp}`;
+    cityTemp.textContent = `Temp : ${cityData.list[index].main.temp}`;
 
     const cityWind = document.createElement('p');
-    cityWind.textContent = `Wind : ${cityData.wind.speed}`;
+    cityWind.textContent = `Wind : ${cityData.list[index].wind.speed}`;
 
     const cityhumidity = document.createElement('p');
-    cityhumidity.textContent = `Humidity : ${cityData.main.humidity}`;
+    cityhumidity.textContent = `Humidity : ${cityData.list[index].main.humidity}`;
 
-    cityEl.append(cityName, weatherIcon, cityTemp, cityWind, cityhumidity);
-    cityContainerEl.append(cityEl);
-};
+    cardBody.append(cityTime, weatherIcon, cityTemp, cityWind, cityhumidity);
+    card.append(cardBody);
+    return card;
+}
 
 
 const formSubmitHandler = function (event) {
@@ -84,7 +113,6 @@ const formSubmitHandler = function (event) {
         cityArray.push(city);
         storeCity();
         showHistory();
-        ;
         cityContainerEl.textContent = '';
         cityInputEl.value = '';
     } else {
@@ -93,11 +121,12 @@ const formSubmitHandler = function (event) {
 };
 
 const buttonClickHandler = function (event) {
-    const cityWeater = event.target.getAttribute('data-city');
+    const city = event.target.getAttribute('data-city');
 
-    if (cityWeater) {
-        getCityWeather(cityWeater);
+    if (city) {
+        getCityWeather(city);
         cityContainerEl.textContent = '';
+        fiveContainer.textContent = '';
     }
 };
 
